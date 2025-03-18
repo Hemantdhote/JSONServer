34a445378508d628e5909df5dfd425a4a847728f
Hemant Dhote <hemantdhote76@gmail.com>
2025-03-18 17:00:06 +0530

    json

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
+
+  function postsFun(){
+    fetch('http://localhost:3000/posts')
+    .then(res=>res.json())
+    .then((posts)=>{
+      console.log(posts)
+    })
+  }
+
+  function usersFun(){
+    fetch('http://localhost:3000/users')
+    .then(res=>res.json())
+    .then((users)=>{
+     console.log(users);
+    })
+  }
+
   return (
-    <div>
-      <h1>hello</h1>
-      
-    </div>
+    <>
+
+      <button onClick={usersFun}>get users</button>
+      <button onClick={postsFun}>get posts</button>
+    </>
+
   )
 }
 
